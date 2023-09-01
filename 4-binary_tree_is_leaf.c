141c5e25a207b2ce5ce7afab60b028fdc496b5f5
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-01 04:21:48 -0700

    hjdjd

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - test if
+ * @node: param
+ * Return: 0 or 1
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+    else
+        return (0);
+}
