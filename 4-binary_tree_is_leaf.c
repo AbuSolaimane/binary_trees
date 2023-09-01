951db1fa09aa7be92d60e46bb3dc0b0275187585
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-01 04:56:47 -0700

    hjdjd

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,6 +10,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 	if (node->left == NULL && node->right == NULL)
 		return (1);
-    else
-        return (0);
+	else
+		return (0);
 }
